300df9876439da314a1ec154cfc5cf7da04e3931
Glitch (qwark) <none>
2024-01-05 23:18:15 +0000

    🚂🚅 Big change : the store is now integrated in Qwark

--- public/null/Docs/app.js
+++ public/null/Docs/app.js
@@ -1,17 +1,8 @@
+   
 
-
-
-
-
-let el = document.getElementById('gameContainer')
-el.innerHTML = `
-
-##################
-# DOSSIER JEROME #
-##################
 
 ---------------
-Qwark Alpha 0.3
+Qwark Alpha 0.2
 ---------------
 La boîte à outils pour créer et
 partager des Apps récréatives
@@ -27,15 +18,14 @@ vers le bas, puis appuye sur bouton de
 sélection vert pour choisir une App
 
 2) Pour lancer l'App :
-utilise le bouton <\> en bas à gauche
+utilise le bouton </> en bas à gauche
 
 3) Pour revenir au code :
-ré-utilise le bouton <\> en bas à gauche
+ré-utilise le bouton </> en bas à gauche
 
 4) Sauvegarde tes changements avec 
 le bouton nuage fleche vers le haut
-dans le cloud (il te faut être connecté
-à internet pour cette étape)
+dans le cloud
 
 5) Partage un lien vers ton App pour permettre
 à d'autres de jouer avec, avec le bouton lien
@@ -43,18 +33,16 @@ en haut à droite
 
 6) Pour créer une nouvelle App à partir d'une
 App existante, duplique le code de l'App courante avec
-le bouton + et donne lui un nouveau nom (une
-connection internet est nécessaire pour cette
-étape)
+le bouton + et donne lui un nouveau nom
 
 
-Tu peux aussi aller dans un autre dossier internet
+Tu peux aussi aller dans un autre dossier 
 que celui où sont rangées tes Apps, avec
 le bouton en forme d'engrenage... mais
 attention à ne pas modifier le dossier de
 quelqu'un d'autre !
 
-N'hésite pas à installer Qwark ! Elle s'executera
+N'hésite pas à installer ton app ! Elle s'executera
 en plein écran plus facilement. Sur iOS, il faut
 appuyer sur le bouton de partage, puis ajouter à l'écran
 d'accueil. Sur OSX/Chrome, une icone à presser représentant
@@ -70,4 +58,3 @@ ps: Ce type d'app est une PWA, une Progressive Web App.
 Amuses toi bien, et partage tes créations !
 
 Avanti !
-`
